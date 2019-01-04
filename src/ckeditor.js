f1975c538de83bd956d5a41646144267127d0c54
Rem <Rem.Kolomna@gmail.com>
2019-01-04 09:16:30 +0300

    feat: base64 images

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -15,10 +15,10 @@ import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Link from '@ckeditor/ckeditor5-link/src/link';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import ImageUploadUI from './imageuploadui';
 
 export default class WebPlannerEditor extends ClassicEditorBase {}
 
@@ -33,7 +33,7 @@ WebPlannerEditor.builtinPlugins = [
 	ImageCaption,
 	ImageStyle,
 	ImageToolbar,
-	ImageUpload,
+	ImageUploadUI,
 	Link,
 	List,
 	Paragraph,
